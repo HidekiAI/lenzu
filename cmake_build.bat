5c1e5152b89f4ea8932cc780932ddcd9c6921097
Hideki A. Ikeda <542972+HidekiAI@users.noreply.github.com>
2024-02-19 13:35:51 -0600

    Removed any CMake related, and now relying on Cargo [workspace] to build sub-projects

--- cmake_build.bat
+++
@@ -1,7 +0,0 @@
-SET _BUILD_DIR=.\\build
-SET _VCPKG_DIR=\\usr\\src\\github\\vcpkg
-
-%_VCPKG_DIR%\\vcpkg install opencv
-cmake -B %_BUILD_DIR% -S . -DCMAKE_TOOLCHAIN_FILE=%_VCPKG_DIR%\\scripts\\buildsystems\\vcpkg.cmake
-cmake --build %_BUILD_DIR%
-
