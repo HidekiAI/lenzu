878ec2b190896ed98509e74797143f3846d61d02
Hideki A. Ikeda <542972+HidekiAI@users.noreply.github.com>
2024-03-21 12:52:17 -0500

    Attempting to integrate rusttype so that overlaying of text can be done in cross-platforms
    - TODO: Fix the issue of overlayed image not getting rendered on the
      window (but debug saved image correctly shows the text overlayed)

--- lenzu/src/ocr_tesseract.rs
+++ lenzu/src/ocr_tesseract.rs
@@ -1,11 +1,50 @@
 use crate::ocr_traits::{self, OcrRect, OcrTrait, OcrTraitResult};
 use anyhow::Error;
-use image::GenericImageView;
+use rusty_tesseract::image::GenericImageView;
 use rusty_tesseract::Args;
 
 // derive from OcrTrait
 pub struct OcrTesseract {
     ocr_args: rusty_tesseract::Args,
+}
+
+impl OcrTesseract {
+    pub fn new_with_args(ocr_args: rusty_tesseract::Args) -> Self {
+        OcrTesseract { ocr_args }
+    }
+
+    //fn to_ocr_rect(rect: ) -> OcrRect {
+    //}
+
+    //fn to_ocr_word(line_index: u16, word: ) -> ocr_traits::OcrWord {
+    //}
+
+    //fn to_ocr_line(
+    //    line_index: u16,
+    //    words:
+    //) -> ocr_traits::OcrLine {
+    //}
+
+    //fn to_ocr_lines(
+    //    lines:
+    //) -> Vec<ocr_traits::OcrLine> {
+    //}
+    fn to_ocr_lines(lines: Vec<String>) -> Vec<ocr_traits::OcrLine> {
+        lines
+            .iter()
+            .enumerate()
+            .map(|(i, line)| {
+                let words = line
+                    .split_whitespace()
+                    .map(|word| {
+                        let rect = OcrRect::new(0, 0, 8, 8);
+                        ocr_traits::OcrWord::new(word.to_string(), i as u16, rect)
+                    })
+                    .collect();
+                ocr_traits::OcrLine::new(words)
+            })
+            .collect()
+    }
 }
 
 impl OcrTrait for OcrTesseract {
@@ -48,14 +87,15 @@ impl OcrTrait for OcrTesseract {
         &self,
         image_path: &str,
     ) -> core::result::Result<ocr_traits::OcrTraitResult, Error> {
-        let img = image::open(image_path).unwrap();
-        self.evaluate(&img)
+        let img = rusty_tesseract::image::open(image_path).unwrap();
+        self.evaluate(&ocr_traits::to_imageproc_dynamic_image(img.as_bytes()))
     }
 
     fn evaluate(
         &self,
-        image: &image::DynamicImage,
+        image: &imageproc::image::DynamicImage,
     ) -> core::result::Result<ocr_traits::OcrTraitResult, Error> {
+        let rusty_image = &ocr_traits::to_rusty_tesseract_dynamic_image(image.as_bytes());
         let supported_lang = rusty_tesseract::get_tesseract_langs().unwrap().join("+");
         // Default OEM=3 (based on what is available)
         // For Manga, PSM should be 6 in gener
@@ -67,7 +107,7 @@ impl OcrTrait for OcrTesseract {
 
         let start_ocr = std::time::Instant::now();
         let ocr_image: Result<rusty_tesseract::Image, rusty_tesseract::TessError> =
-            rusty_tesseract::Image::from_dynamic_image(image); // from_dynamic_image(&gray_scale_image);
+            rusty_tesseract::Image::from_dynamic_image(rusty_image); // from_dynamic_image(&gray_scale_image);
         let ocr_result: Result<String, rusty_tesseract::TessError> = match ocr_image {
             Ok(img) => rusty_tesseract::image_to_string(&img, &ocr_args),
             Err(e) => {
@@ -80,20 +120,12 @@ impl OcrTrait for OcrTesseract {
         let ocr_str = ocr_result.unwrap();
         let x_min = 0;
         let y_min = 0;
+        let lines = vec![ocr_str.split("\n").collect()];
+        let rect_lines: Vec<ocr_traits::OcrLine> = Self::to_ocr_lines(lines.clone());
         let result = OcrTraitResult {
             text: ocr_str.clone(),
-            lines: vec![ocr_str.split("\n").collect()],
-            rects: vec![(
-                OcrRect::new(
-                    x_min,
-                    y_min,
-                    x_min + image.width() as i32,
-                    y_min + image.height() as i32,
-                ),
-                vec![ocr_str.split("\n").collect()],
-            )]
-            .into_iter()
-            .collect(),
+            lines: lines.clone(),
+            rects: rect_lines,
         };
         Ok(result)
     }
--- lenzu/src/ocr_traits.rs
+++ lenzu/src/ocr_traits.rs
@@ -1,10 +1,17 @@
 use anyhow::Error; // the most easiest way to handle errors
 use core::result::Result;
-use image::DynamicImage;
 use std::{
     collections::HashMap,
     fmt::{self, Display, Formatter},
 };
+
+// because there seems to be mismatch on the types of images, we need to convert the image to the
+pub fn to_imageproc_dynamic_image(image: &[u8]) -> imageproc::image::DynamicImage {
+    imageproc::image::load_from_memory(image).unwrap()
+}
+pub fn to_rusty_tesseract_dynamic_image(image: &[u8]) -> rusty_tesseract::image::DynamicImage {
+    rusty_tesseract::image::load_from_memory(image).unwrap()
+}
 
 pub trait OcrTrait {
     // without 'Sized',  won't be able to Box<dyn OcrTrait>
@@ -18,7 +25,7 @@ pub trait OcrTrait {
 
     fn evaluate_by_paths(&self, image_path: &str) -> Result<OcrTraitResult, Error>;
 
-    fn evaluate(&self, image: &DynamicImage) -> Result<OcrTraitResult, Error>;
+    fn evaluate(&self, image: &imageproc::image::DynamicImage) -> Result<OcrTraitResult, Error>;
 }
 
 //
@@ -57,32 +64,115 @@ impl OcrRect {
     }
 }
 
+// A word is a collection (one or more) of characters and its position
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub(crate) struct OcrWord {
+    word: String,
+    line_index: u16,
+    rect: OcrRect,
+}
+impl OcrWord {
+    pub fn new(word: String, line_index: u16, rect: OcrRect) -> Self {
+        OcrWord {
+            word,
+            line_index,
+            rect,
+        }
+    }
+    pub fn from(
+        word: String,
+        line_index: u16,
+        x_min: i32,
+        y_min: i32,
+        width: u32,
+        height: u32,
+    ) -> Self {
+        OcrWord {
+            word,
+            line_index,
+            rect: OcrRect::from(x_min, y_min, width, height),
+        }
+    }
+    pub fn width(&self) -> u32 {
+        self.rect.width()
+    }
+    pub fn height(&self) -> u32 {
+        self.rect.height()
+    }
+    pub fn x_min(&self) -> i32 {
+        self.rect.x_min
+    }
+    pub fn y_min(&self) -> i32 {
+        self.rect.y_min
+    }
+    pub fn rect(&self) -> OcrRect {
+        self.rect
+    }
+    pub fn line_index(&self) -> u16 {
+        self.line_index
+    }
+    pub fn word(&self) -> String {
+        self.word.clone()
+    }
+}
+
+// a line is a collection (one or more) of words
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub(crate) struct OcrLine {
+    line: Vec<OcrWord>,
+}
+impl OcrLine {
+    pub fn new(words: Vec<OcrWord>) -> Self {
+        OcrLine { line: words }
+    }
+    pub fn add_word(&mut self, word: OcrWord) {
+        self.line.push(word);
+    }
+    pub fn words(&self) -> Vec<OcrWord> {
+        self.line.clone()
+    }
+    pub fn width(&self) -> u32 {
+        // get the largest/max width of the words in the line
+        self.line
+            .iter()
+            .fold(0, |acc, word| std::cmp::max(acc, word.width()))
+    }
+    pub fn height(&self) -> u32 {
+        // max height of the words in the line
+        self.line
+            .iter()
+            .fold(0, |acc, word| std::cmp::max(acc, word.height()))
+    }
+    pub fn x_min(&self) -> i32 {
+        // lowest/mimumum x_min of the words in the line
+        self.line
+            .iter()
+            .fold(std::i32::MAX, |acc, word| std::cmp::min(acc, word.x_min()))
+    }
+    pub fn y_min(&self) -> i32 {
+        self.line
+            .iter()
+            .fold(std::i32::MAX, |acc, word| std::cmp::min(acc, word.y_min()))
+    }
+}
+
 #[derive(Debug)]
 pub(crate) struct OcrTraitResult {
-    pub text: String,       // entier text split via newlines (built from lines)
+    pub text: String,        // entier text split via newlines (built from lines)
     pub lines: Vec<String>, // each line of text (collection of words), sequentially ordered (up to OCR whether it is horizontal:left-to-right, or vertical:top-to-bottom-left-to-right )
-    pub rects: HashMap<OcrRect, Vec<String>>, // for each (rectangle) block of text (collection of words, see lines)
+    pub rects: Vec<OcrLine>, // for each (rectangle) block of text (collection of words, see lines)
 }
 
 impl Display for OcrTraitResult {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        let str_lines = self.lines.join("\n");
-        // join all words into single  line, and split  each rects into newlines
-        let str_rects = self
-            .rects
+        let lines = self
+            .lines
             .iter()
-            .fold("".to_string(), |acc, (rect, words)| {
-                // make the collection of words into single line without any separations (this becomes tricky for English, but for
-                // Japanese, it is trivial since there are no such thing as spaces, hence there are tools such as mecab that tries
-                // to interpret where to separate for speach and dictionary lookups...)
-                format!("{}\n{:?}:{}", acc, rect, words.join(" ")) // previous lines, concatinated with new line in format of rect:words
-            });
-
-        write!(
-            f,
-            "OcrTraitResult {{\n text: {:?}\n lines: {:?}\n rects: {:?}\n }}",
-            self.text, str_lines, str_rects
-        )
+            .fold("".to_string(), |acc, line| acc + line + "\n");
+        let rects = self.rects.iter().fold("".to_string(), |acc, rect| {
+            acc + &format!("{:?}", rect) + "\n"
+        });
+        write!(f, "text:{}\nlines:{}\nrects:{}", self.text, lines, rects)
     }
 }
 
@@ -93,7 +183,7 @@ impl OcrTraitResult {
         OcrTraitResult {
             text: "".to_string(),
             lines: vec![],
-            rects: HashMap::new(),
+            rects: vec![],
         }
     }
 }
