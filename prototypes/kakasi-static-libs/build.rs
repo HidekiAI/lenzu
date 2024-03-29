05af5222a2f5360c468d983b3f44380035ef2d7f
Hideki A. Ikeda <542972+HidekiAI@users.noreply.github.com>
2024-03-29 17:05:08 -0500

    Removed iconv libs from local lib dir, it was clashing on linker due to it; each dev host MUST make sure to install iconv lib to build this crate

--- prototypes/kakasi-static-libs/build.rs
+++ prototypes/kakasi-static-libs/build.rs
@@ -1,8 +1,10 @@
 extern crate bindgen;
 fn main() {
+    println!("cargo:rerun-if-env-changed=RUSTFLAGS");
+
     // Specify the path to the libkakasi header file
-    let include_path= if cfg!(target_os = "windows") {
-        ".\\mingw-x86_64-libs\\libkakasi.h"
+    let include_path = if cfg!(target_os = "windows") {
+        "./lib/libkakasi.h"
     } else {
         "/usr/include/libkakasi.h"
     };
@@ -10,12 +12,31 @@ fn main() {
     // Set library paths based on the target platform and statically link:
     if cfg!(target_os = "windows") {
         // NOTE/IMPORTANT: The path to the library is relative to the project root!!!!!!
-        println!("cargo:rustc-link-search=native=prototypes/kakasi-static-libs/mingw-x86_64-libs");
+        println!(
+            "cargo:rustc-link-search=native=prototypes/kakasi-static-libs/lib/x86_64-w64-mingw32"
+        );
     } else {
         println!("cargo:rustc-link-search=native=/usr/lib");
     }
-    println!("cargo:rustc-link-lib=static=kakasi");
-    println!("cargo:rustc-link-arg=-liconv");
+    // as the project name states, we are linking against the static library!
+    println!("cargo:rustc-link-lib=static=kakasi"); // link against libkakasi.a and/or kakasi.dll/.so (possibly, I'd want "-l:libkakasi.a" instead of "-lkakasi)
+    println!("cargo:rustc-link-arg=-lkakasi");    // force link against libkakasi.a (statically linked)
+    //println!("cargo:rustc-link-arg=-l:libkakasi.a");    // force link against libkakasi.a (statically linked)
+
+    // Other libs that libkakasi.a depends on:
+    println!("cargo:rustc-link-arg=-liconv");    // force link against libkakasi.a (statically linked)
+
+    // Set RUSTFLAGS to include the desired linker search path
+    if cfg!(target_os = "windows") {
+        // Unlike Linux, MinGW can get a bit confusing on where the lib files are located...
+        println!("cargo:rustc-link-arg=-L/mingw64/lib/");
+        println!("cargo:rustc-link-arg=-L/c/msys64/mingw64/lib/");
+        println!("cargo:rustc-link-arg=-LC:/msys64/mingw64/lib/");
+        println!("cargo:rustc-link-arg=-L/usr/lib/");
+        println!("cargo:rustc-link-arg=-LC:/msys64/usr/lib/");
+    }
+    println!("cargo:rustc-link-arg=-lm");
+    println!("cargo:rustc-link-arg=-static");
 
     let bindings = bindgen::Builder::default()
         .header(include_path)
