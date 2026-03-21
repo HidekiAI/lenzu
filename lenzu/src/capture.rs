2713834b517d4ce81cf90e8d582df4efd49e8807
Hideki A. Ikeda <hidekiai@CodeMonkeyNinja.dev@users.noreply.github.com>
2026-03-21 14:38:22 -0500

    Phase 1: refactoring to make it more easier to unit-test

---
+++ lenzu/src/capture.rs
@@ -0,0 +1,23 @@
+use x11rb::connection::Connection;
+use x11rb::protocol::xproto::{ConnectionExt, ImageFormat};
+use x11rb::rust_connection::RustConnection;
+
+pub fn capture_x11(x: i32, y: i32, w: u32, h: u32) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let (conn, screen_num) = RustConnection::connect(None)?;
+    let screen = &conn.setup().roots[screen_num];
+    let root = screen.root;
+
+    let reply = conn
+        .get_image(
+            ImageFormat::Z_PIXMAP,
+            root,
+            x as i16,
+            y as i16,
+            w as u16,
+            h as u16,
+            0xffffffff,
+        )?
+        .reply()?;
+
+    Ok(reply.data)
+}
